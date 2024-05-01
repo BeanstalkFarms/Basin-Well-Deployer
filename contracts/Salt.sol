c2632fcbff67baaa339ec867762c6b0af0bc6c13
nickkatsios <t8200071@aueb.gr>
2024-05-01 17:35:55 +0300

    Adjust mine salt script

--- contracts/Salt.sol
+++ contracts/Salt.sol
@@ -2,31 +2,28 @@
 pragma solidity ^0.8.20;
 
 import {LibClone} from "./LibClone.sol";
-import "hardhat/console.sol";
 
-contract MockSalt {
+contract Salt {
     using LibClone for address;
 
-    address wellDeployer;
-    // string targetPrefix;
-    uint256 maxIterations;
-    uint256 constant prefixCharactersLength = 2;
-
+    address constant WELL_DEPLOYER = 0x7eaE23DD0f0d8289d38653BCE11b92F7807eFB64; // TODO: Set
     address constant AQUIFER = 0xBA51AAAA95aeEFc1292515b36D86C51dC7877773;
     address constant WELL_IMPLEMENTATION = 0xBA510e11eEb387fad877812108a3406CA3f43a4B;
     bytes INIT_DATA =
         hex"ba51aaaa95aeefc1292515b36d86c51dc78777730000000000000000000000000000000000000000000000000000000000000002ba510c20fd2c52e4cb0d23cfc3ccd092f9165a6e00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000bea0000029ad1c77d3d5d23ba2d8893db9d1efab000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2ba510f10e3095b83a0f33aa9ad2544e22570a87c0000000000000000000000000000000000000000000000000000000000000000";
-    
-    bytes constant targetPrefix=hex"ab";
 
-    constructor(address _wellDeployer, uint256 _maxIterations) {
-        wellDeployer = _wellDeployer;
-        maxIterations = _maxIterations;
+    string constant targetPrefix = "ab";
+    uint256 CHARACTERS = 2;
+
+    uint256 attemptsPerRun;
+
+    constructor(uint256 _attemptsPerRun) {
+        attemptsPerRun = _attemptsPerRun;
     }
 
     function checkForAddress(uint256 start) external view returns (address a, string memory str, bytes32 s) {
         bytes32 targetHash = keccak256(abi.encodePacked(targetPrefix));
-        uint256 end = start + maxIterations;
+        uint256 end = start + attemptsPerRun;
         for (uint256 i = start; i < end; i++) {
             (a, str) = checkSalt(bytes32(uint256(i)), targetHash);
             if (a != address(0)) {
@@ -38,8 +35,8 @@ contract MockSalt {
 
     function checkSalt(bytes32 salt, bytes32 targetHash) internal view returns (address, string memory) {
         bytes memory initData = INIT_DATA;
-        address well = predictWellAddress(AQUIFER, wellDeployer, WELL_IMPLEMENTATION, initData, salt);
-        string memory wellString = toHexString(well, prefixCharactersLength);
+        address well = predictWellAddress(AQUIFER, WELL_DEPLOYER, WELL_IMPLEMENTATION, initData, salt);
+        string memory wellString = toHexString(well, CHARACTERS);
 
         if (keccak256(abi.encodePacked(wellString)) == targetHash) {
             return (well, wellString);
--- scripts/mineSalt.js
+++ scripts/mineSalt.js
@@ -1,14 +1,24 @@
 const hre = require("hardhat");
-const inquirer = require('inquirer');
+const fs = require("fs");
 
 async function mineSalt() {
+    const saltCheckerFactory = await hre.ethers.getContractFactory("Salt");
+    const attemptsPerRun = 100;
+    const saltChecker = await saltCheckerFactory.deploy(attemptsPerRun);
     for (let i = 0; i < 10; i++) {
-        console.log(`Iteration: ${i * 5_000} through ${(i+1) * 5_000}`);
-        const saltCheckerFactory = await hre.ethers.getContractFactory("Salt");
-        const saltChecker = await saltCheckerFactory.deploy();
-        const start = i * 5_000;
+        console.time(`Iteration ${i}`);
+        console.log(`Iteration: ${i * attemptsPerRun} through ${(i+1) * attemptsPerRun}`);
+        const start = i * attemptsPerRun
         const result = await saltChecker.checkForAddress(start, { gasLimit: 5000000000 });
-        console.log(result);
+        if (result[0] !== hre.ethers.ZeroAddress) {
+            console.log(`Predicted Well address: ${result[0]}`);
+            console.log(`Prefix: ${result[1]}`);
+            console.log(`Salt: ${result[2]}`);
+            // write to file
+            fs.writeFileSync(`./data/well.txt`, `Found Well address: ${result[0]}\nPrefix: ${result[1]}\nSalt: ${result[2]}`);
+            break;
+        }
+        console.timeEnd(`Iteration ${i}`);
     }
 }
 
